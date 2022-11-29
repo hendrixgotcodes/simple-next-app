566c6b7d786c8ad48f39ed24ed9a2eee89c1882a
Samuel O. Asare <asare11samuel@gmail.com>
2022-11-29 22:17:18 +0000

    feat: using getServerSideProps

--- pages/article/[id]/index.tsx
+++ pages/article/[id]/index.tsx
@@ -1,12 +1,28 @@
 import React from 'react'
 import { useRouter } from 'next/router'
+import Link from 'next/link'
 
-export default function ArticleDetails() {
+export default function ArticleDetails({article}:{article: {[key:string]:any}}) {
 
     const router = useRouter()
     const {id} = router.query
     
   return (
-    <div>This is article {id}</div>
+    <>
+        <h1>{article.title}</h1>
+        <p>{article.body}</p>
+        <br />
+        <Link href="/">Go back</Link>
+    </>
   )
+}
+
+export const getServerSideProps = async (context:any)=>{
+    const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${context.params.id}`)
+    const article = await res.json()
+    return {
+        props:{
+            article
+        }
+    }
 }
